0fba9e1801c786f95700c3d539bd55ff422ba02c
Gaurav Tanwar <gaurav.tanwar19@bvicam.in>
2021-05-07 20:11:16 +0530

    Deploy Website

---
+++ build/precache-manifest.ef8b983f4bef340ab4b75309f5bd7c44.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87350b0ce607ab35eb25ac6d41b067e6",
+    "url": "/React-To-Do-List/index.html"
+  },
+  {
+    "revision": "19e6aa8231c08963f203",
+    "url": "/React-To-Do-List/static/js/2.dbb238c8.chunk.js"
+  },
+  {
+    "revision": "a5fb6cc1adb644a0e337",
+    "url": "/React-To-Do-List/static/js/main.316dffb3.chunk.js"
+  },
+  {
+    "revision": "e32118474552fb4f4cdd",
+    "url": "/React-To-Do-List/static/js/runtime-main.cac2cd32.js"
+  }
+]);
